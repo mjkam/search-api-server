e89ba955b822c4a987eaa644da01fe564e187853
mjkam <mjkam@naver.com>
2023-03-19 14:50:03 +0900

    improve(external): add field to deal with different sorting parameter name of providers

--- src/main/java/com/mjkam/search/external/SortingType.java
+++ src/main/java/com/mjkam/search/external/SortingType.java
@@ -1,5 +1,16 @@
 package com.mjkam.search.external;
 
+import lombok.Getter;
+
+@Getter
 public enum SortingType {
-    ACCURACY, RECENCY
+    ACCURACY("accuracy"),
+    RECENCY("recency"),
+    ;
+
+    private final String kakaoName;
+
+    SortingType(String kakaoName) {
+        this.kakaoName = kakaoName;
+    }
 }
