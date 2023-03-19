717630d58496d6159839fd7ca25bfc39f297940f
mjkam <mjkam@naver.com>
2023-03-19 12:58:05 +0900

    new(external): add fields for external api request parameters

--- src/main/java/com/mjkam/search/external/BlogSearchRequest.java
+++ src/main/java/com/mjkam/search/external/BlogSearchRequest.java
@@ -1,4 +1,15 @@
 package com.mjkam.search.external;
 
 public class BlogSearchRequest {
+    private final String query;
+    private final Integer page;
+    private final Integer size;
+    private final SortingType sortingType;
+
+    public BlogSearchRequest(String query, Integer page, Integer size, SortingType sortingType) {
+        this.query = query;
+        this.page = page;
+        this.size = size;
+        this.sortingType = sortingType;
+    }
 }
