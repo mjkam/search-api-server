27fe6ac82b4f9b1033b61c2092e936b1edbc7c70
mjkam <mjkam@naver.com>
2023-03-19 18:08:01 +0900

    feat(external): add Kakao provider requester

---
+++ src/main/java/com/mjkam/search/external/provider/kakao/KakaoBlogSearchApiRequester.java
@@ -0,0 +1,46 @@
+package com.mjkam.search.external.provider.kakao;
+
+import com.mjkam.search.external.BlogSearchApiException;
+import com.mjkam.search.external.BlogSearchApiRequester;
+import com.mjkam.search.external.BlogSearchApiResponse;
+import com.mjkam.search.external.BlogSearchRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
+public class KakaoBlogSearchApiRequester implements BlogSearchApiRequester {
+    private final RestTemplate restTemplate;
+    private final KakaoConfiguration kakaoConfiguration;
+
+    @Override
+    public BlogSearchApiResponse execute(BlogSearchRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, String.format("KakaoAK %s", kakaoConfiguration.getKey()));
+        HttpEntity<Object> header = new HttpEntity<>(headers);
+
+        String uri = UriComponentsBuilder.fromUriString(kakaoConfiguration.getUrl())
+                .queryParam("query", request.getQuery())
+                .queryParam("page", request.getPage())
+                .queryParam("size", request.getSize())
+                .queryParam("sort", request.getSortingType().getKakaoName())
+                .toUriString();
+
+        ResponseEntity<KakaoApiResponse> response =
+                restTemplate.exchange(uri, HttpMethod.GET, header, KakaoApiResponse.class);
+
+        KakaoApiResponse responseBody = response.getBody();
+        if (responseBody == null) {
+            throw new BlogSearchApiException("Body should be not null");
+        }
+
+        return BlogSearchApiResponse.fromKakao(
+                request.getPage(), request.getSize(), responseBody);
+    }
+}
---
+++ src/test/java/com/mjkam/search/external/provider/kakao/KakaoBlogSearchApiRequesterTest.java
@@ -0,0 +1,90 @@
+package com.mjkam.search.external.provider.kakao;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mjkam.search.external.BlogSearchApiResponse;
+import com.mjkam.search.external.BlogSearchRequest;
+import com.mjkam.search.external.SortingType;
+import com.mjkam.search.external.provider.kakao.support.DummyKakaoDocumentCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static com.mjkam.search.external.provider.kakao.support.KakaoApiResponseBuilder.kakaoApiResponse;
+import static com.mjkam.search.external.provider.kakao.support.KakaoMetaBuilder.kakaoMeta;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+
+public class KakaoBlogSearchApiRequesterTest {
+    private KakaoBlogSearchApiRequester sut;
+    private MockRestServiceServer mockRestServiceServer;
+
+    private final String API_KEY = "TEMP_API_KEY";
+    private final String API_ENDPOINT = "http://test.com";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setup() {
+        RestTemplate restTemplate = new RestTemplate();
+        KakaoConfiguration kakaoConfiguration = new KakaoConfiguration();
+        kakaoConfiguration.setKey(API_KEY);
+        kakaoConfiguration.setUrl(API_ENDPOINT);
+
+        mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
+        sut = new KakaoBlogSearchApiRequester(restTemplate, kakaoConfiguration);
+    }
+
+    @Test
+    @DisplayName("kakao api 정상 호출 테스트")
+    void callKakaoApiNormalTest() throws JsonProcessingException {
+        //given
+        String query = "TEST";
+        int page = 10;
+        int size = 10;
+        SortingType sortingType = SortingType.ACCURACY;
+
+        int totalCount = 100000000;
+        int pageableCount = 800;
+        KakaoMeta meta =
+                kakaoMeta()
+                        .totalCount(totalCount)
+                        .pageableCount(pageableCount)
+                        .build();
+        List<KakaoDocument> kakaoDocuments = DummyKakaoDocumentCreator.create(pageableCount, page, size);
+
+        KakaoApiResponse kakaoApiResponse =
+                kakaoApiResponse()
+                        .kakaoMeta(meta)
+                        .documents(kakaoDocuments)
+                        .build();
+
+        mockRestServiceServer
+                .expect(requestTo(url(query, page, size, sortingType)))
+                .andExpect(header(HttpHeaders.AUTHORIZATION, String.format("KakaoAK %s", API_KEY)))
+                .andRespond(MockRestResponseCreators.withSuccess(objectMapper.writeValueAsString(kakaoApiResponse), MediaType.APPLICATION_JSON));
+
+        BlogSearchRequest request = new BlogSearchRequest(query, page, size, sortingType);
+
+        //when
+        BlogSearchApiResponse response = sut.execute(request);
+
+        //then
+        assertThat(response.getTotalCount()).isEqualTo(totalCount);
+        assertThat(response.getPageableCount()).isEqualTo(800);
+        assertThat(response.getDocuments().size()).isEqualTo(10);
+    }
+
+    private String url(String query, int page, int size, SortingType sortingType) {
+        return String.format("%s?query=%s&page=%d&size=%d&sort=%s",
+                API_ENDPOINT, query, page, size, sortingType.getKakaoName());
+    }
+}
