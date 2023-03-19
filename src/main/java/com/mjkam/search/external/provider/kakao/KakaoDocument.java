d21cdd5b11e072711839a91da133659f0e330b0c
mjkam <mjkam@naver.com>
2023-03-19 18:54:23 +0900

    fix(external): add JsonProperty annotation to parse kakao api response

--- src/main/java/com/mjkam/search/external/provider/kakao/KakaoDocument.java
+++ src/main/java/com/mjkam/search/external/provider/kakao/KakaoDocument.java
@@ -1,6 +1,7 @@
 package com.mjkam.search.external.provider.kakao;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -12,6 +13,7 @@ public class KakaoDocument {
     private String title;
     private String contents;
     private String url;
+    @JsonProperty("blogname")
     private String blogName;
     private String thumbnail;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
