9c0bed38d8646d516356a0a3bf7db4015c4a0417
mjkam <mjkam@naver.com>
2023-03-19 14:52:41 +0900

    new(external): add exception for ApiRequester

---
+++ src/main/java/com/mjkam/search/external/BlogSearchApiException.java
@@ -0,0 +1,7 @@
+package com.mjkam.search.external;
+
+public class BlogSearchApiException extends RuntimeException {
+    public BlogSearchApiException(String message) {
+        super(message);
+    }
+}
