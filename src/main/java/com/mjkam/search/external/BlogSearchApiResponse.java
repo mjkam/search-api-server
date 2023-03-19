e28f7e1412d940b444bf44b2f047e24afe37a857
mjkam <mjkam@naver.com>
2023-03-19 17:33:28 +0900

    improve(external): add mapping method to map kakaoResponse to BlogSearchApiResponse

--- src/main/java/com/mjkam/search/external/BlogSearchApiResponse.java
+++ src/main/java/com/mjkam/search/external/BlogSearchApiResponse.java
@@ -1,6 +1,9 @@
 package com.mjkam.search.external;
 
+import com.mjkam.search.external.provider.kakao.KakaoApiResponse;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BlogSearchApiResponse {
     private final int totalCount;
@@ -11,5 +14,14 @@ public class BlogSearchApiResponse {
         this.totalCount = totalCount;
         this.pageableCount = pageableCount;
         this.documents = blogInfos;
+    }
+
+    public static BlogSearchApiResponse fromKakao(int page, int size, KakaoApiResponse response) {
+        List<BlogInfo> blogDocuments = response.getDocumentsForResponse(page, size).stream()
+                .map(BlogInfo::of)
+                .collect(Collectors.toList());
+
+        return new BlogSearchApiResponse(
+                response.getTotalCount(), response.getPageableCount(), blogDocuments);
     }
 }
