eb3a1531028f1911cd13a4294dc0102ea7df3fc0
mjkam <mjkam@naver.com>
2023-03-20 16:05:56 +0900

    refactor(test): extract method for mockApiServer to improve readability

--- src/test/java/com/mjkam/search/external/provider/kakao/KakaoBlogSearchApiRequesterTest.java
+++ src/test/java/com/mjkam/search/external/provider/kakao/KakaoBlogSearchApiRequesterTest.java
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mjkam.search.external.BlogSearchApiResponse;
 import com.mjkam.search.external.BlogSearchRequest;
+import com.mjkam.search.external.ExternalBaseTest;
 import com.mjkam.search.external.SortingType;
 import com.mjkam.search.external.provider.kakao.support.KakaoApiResponseCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpHeaders;
@@ -21,22 +21,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 
-public class KakaoBlogSearchApiRequesterTest {
+public class KakaoBlogSearchApiRequesterTest extends ExternalBaseTest {
     private KakaoBlogSearchApiRequester sut;
     private MockRestServiceServer mockRestServiceServer;
-
-    private final String API_KEY = "TEMP_API_KEY";
-    private final String API_ENDPOINT = "http://test.com";
-    private final String query = "TEST";
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setup() {
         RestTemplate restTemplate = new RestTemplate();
         KakaoConfiguration kakaoConfiguration = new KakaoConfiguration();
-        kakaoConfiguration.setKey(API_KEY);
-        kakaoConfiguration.setUrl(API_ENDPOINT);
+        kakaoConfiguration.setKey(DUMMY_KAKAO_API_KEY);
+        kakaoConfiguration.setUrl(DUMMY_API_ENDPOINT);
 
         mockRestServiceServer = MockRestServiceServer.createServer(restTemplate);
         sut = new KakaoBlogSearchApiRequester(restTemplate, kakaoConfiguration);
@@ -46,33 +40,37 @@ public class KakaoBlogSearchApiRequesterTest {
     @DisplayName("kakao api 정상 호출 테스트")
     @CsvSource(delimiter = ':',
             value = {
-                    "80000:100:10:10:10",
-                    "100:100:9:7:7",
-                    "200:100:15:9:0",
-                    "3000:13:2:7:6"})
-    void callKakaoApiNormalTest(int totalCount, int pageableCount, int page, int size, int resultDocumentCount) throws JsonProcessingException {
+                    "10:10:80000:800:10",
+                    "9:7:100:100:7",
+                    "15:9:200:100:0",
+                    "2:7:100:13:6"
+            })
+    void callKakaoApiNormalTest(int page, int size, int totalCountFromServer, int pageableCountFromServer, int expectDocumentCount) throws JsonProcessingException {
         //given
-        SortingType sortingType = SortingType.ACCURACY;
-        BlogSearchRequest request = new BlogSearchRequest(query, page, size, sortingType);
-
-        KakaoApiResponse kakaoApiResponse = KakaoApiResponseCreator.create(totalCount, pageableCount, page, size);
-
-        mockRestServiceServer
-                .expect(requestTo(url(query, page, size, sortingType)))
-                .andExpect(header(HttpHeaders.AUTHORIZATION, String.format("KakaoAK %s", API_KEY)))
-                .andRespond(MockRestResponseCreators.withSuccess(objectMapper.writeValueAsString(kakaoApiResponse), MediaType.APPLICATION_JSON));
+        BlogSearchRequest request = new BlogSearchRequest(DUMMY_QUERY, page, size, DUMMY_SORTING_TYPE);
+        mockKakaoApiServer(totalCountFromServer, pageableCountFromServer, page, size);
 
         //when
         BlogSearchApiResponse response = sut.execute(request);
 
         //then
-        assertThat(response.getTotalCount()).isEqualTo(totalCount);
-        assertThat(response.getPageableCount()).isEqualTo(pageableCount);
-        assertThat(response.getDocuments().size()).isEqualTo(resultDocumentCount);
+        assertThat(response.getTotalCount()).isEqualTo(totalCountFromServer);
+        assertThat(response.getPageableCount()).isEqualTo(pageableCountFromServer);
+        assertThat(response.getDocuments().size()).isEqualTo(expectDocumentCount);
+    }
+
+    private void mockKakaoApiServer(int totalCountFromServer, int pageableCountFromServer, int page, int size) throws JsonProcessingException {
+        KakaoApiResponse kakaoApiResponse =
+                KakaoApiResponseCreator.createKakaoResponse(totalCountFromServer, pageableCountFromServer, page, size);
+
+        mockRestServiceServer
+                .expect(requestTo(url(DUMMY_QUERY, page, size, DUMMY_SORTING_TYPE)))
+                .andExpect(header(HttpHeaders.AUTHORIZATION, String.format("KakaoAK %s", DUMMY_KAKAO_API_KEY)))
+                .andRespond(MockRestResponseCreators.withSuccess(objectMapper.writeValueAsString(kakaoApiResponse), MediaType.APPLICATION_JSON));
     }
 
     private String url(String query, int page, int size, SortingType sortingType) {
         return String.format("%s?query=%s&page=%d&size=%d&sort=%s",
-                API_ENDPOINT, query, page, size, sortingType.getKakaoName());
+                DUMMY_API_ENDPOINT, query, page, size, sortingType.getKakaoName());
     }
 }
